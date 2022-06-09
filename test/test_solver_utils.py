a1e6b054b3503d4188c1f93a187c920b306af8b3
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2022-06-09 20:16:27 +0100

    test: add test for get_input_variables function

--- test/test_solver_utils.py
+++ test/test_solver_utils.py
@@ -1,7 +1,8 @@
 import unittest
 import pandas as pd
 import numpy as np
-from solver_utils import get_input_domain_and_bounds
+from docplex.mp.model import Model
+from solver_utils import get_input_domain_and_bounds, get_input_variables
 
 
 class TestSolverUtils(unittest.TestCase):
@@ -15,6 +16,24 @@ class TestSolverUtils(unittest.TestCase):
         domain, bounds = get_input_domain_and_bounds(dataframe)
         self.assertEqual(domain, ['I', 'C', 'B'])
         self.assertTrue(np.array_equal(bounds, np.array([[18, 20], [58.0, 89.5], [0, 1]])))
+
+    def test_get_input_variables(self):
+        mdl = Model()
+        domain = ['I', 'C', 'B']
+        bounds = np.array([[18, 20], [58.0, 89.5], [0, 1]])
+        input_variables = get_input_variables(mdl, domain, bounds)
+        self.assertTrue(input_variables[0].is_integer())
+        self.assertEqual(input_variables[0].name, 'x_0')
+        self.assertEqual(input_variables[0].lb, bounds[0][0])
+        self.assertEqual(input_variables[0].ub, bounds[0][1])
+        self.assertTrue(input_variables[1].is_continuous())
+        self.assertEqual(input_variables[1].name, 'x_1')
+        self.assertEqual(input_variables[1].lb, bounds[1][0])
+        self.assertEqual(input_variables[1].ub, bounds[1][1])
+        self.assertTrue(input_variables[2].is_binary())
+        self.assertEqual(input_variables[2].name, 'x_2')
+        self.assertEqual(input_variables[2].lb, bounds[2][0])
+        self.assertEqual(input_variables[2].ub, bounds[2][1])
 
 
 if __name__ == '__main__':
