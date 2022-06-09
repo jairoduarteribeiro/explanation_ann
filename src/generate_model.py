2e9e9fd061741b803224624a63f83e10ead1eb7a
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2022-06-10 00:15:18 +0100

    feat: train iris model

--- src/generate_model.py
+++ src/generate_model.py
@@ -1,5 +1,5 @@
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Input
+from tensorflow.keras.layers import Dense, InputLayer
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.losses import SparseCategoricalCrossentropy
 from tensorflow.keras.metrics import SparseCategoricalAccuracy
@@ -7,21 +7,21 @@ from tensorflow.keras.activations import relu, softmax
 from tensorflow.keras.callbacks import EarlyStopping, ModelCheckpoint
 from models.model_utils import get_model_path
 from time import time
-from datasets.heart_disease import heart
+from datasets.iris import iris
 
-# Load Glass dataset and normalize it
-(x_train, y_train), (x_test, y_test) = heart.load_data()
+# Load IRIS dataset and normalize it
+(x_train, y_train), (x_test, y_test) = iris.load_data()
 
 # Parameters
-n_classes = 2
+n_classes = 3
 n_hidden_layers = 4
-n_neurons = 32
+n_neurons = 16
 n_epochs = 100
 batch_size = 4
 
 # Create model
-model = Sequential([Input(shape=(x_train.shape[1],))])
-for _ in range(n_hidden_layers):
+model = Sequential([InputLayer(input_shape=(x_train.shape[1],))])
+for _ in range(n_hidden_layers - 1):
     model.add(Dense(n_neurons, activation=relu))
 model.add(Dense(n_classes, activation=softmax))
 model.compile(optimizer=Adam(),
@@ -30,7 +30,7 @@ model.compile(optimizer=Adam(),
 
 # Callbacks
 early_stopping = EarlyStopping(patience=20)
-model_checkpoint = ModelCheckpoint(filepath=get_model_path('heart.h5'),
+model_checkpoint = ModelCheckpoint(filepath=get_model_path('iris.h5'),
                                    save_best_only=True)
 
 if __name__ == '__main__':
