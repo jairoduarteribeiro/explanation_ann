ea9b40475bbda0735ca88cf24b7cd11b288bc0ad
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2022-06-10 01:49:37 +0100

    feat: print explanation

--- src/explanation.py
+++ src/explanation.py
@@ -2,7 +2,15 @@ from fischetti import insert_output_constraints_fischetti
 from tjeng import insert_tjeng_output_constraints
 
 
-def get_minimal_explanation(mdl, output_bounds, method, network_input, network_output, number_classes):
+def print_explanation(x_idx, x_columns, explanation):
+    def constraint_to_idx(constraint):
+        return constraint.lhs.name
+    columns = [constraint_to_idx(constraint) for constraint in explanation]
+    print(f'Explanation for data {x_idx}: {columns}')
+
+
+def get_minimal_explanation(mdl, output_bounds, method, network_input, network_output):
+    number_classes = len(output_bounds)
     variables = {
         'output': [mdl.get_var_by_name(f'o_{class_idx}') for class_idx in range(number_classes)],
         'binary': mdl.binary_var_list(number_classes - 1, name='b')
@@ -17,7 +25,7 @@ def get_minimal_explanation(mdl, output_bounds, method, network_input, network_o
         insert_output_constraints_fischetti(mdl, network_output, variables)
     for constraint in input_constraints:
         mdl.remove_constraint(constraint)
-        mdl.solve(log_output=True)
+        mdl.solve(log_output=False)
         if mdl.solution is not None:
             mdl.add_constraint(constraint)
     return mdl.find_matching_linear_constraints('input')
--- src/main.py
+++ src/main.py
@@ -5,24 +5,24 @@ from tensorflow.keras.models import load_model
 from datasets.dataset_utils import get_dataset_path
 from models.model_utils import get_model_path
 from milp import build_network
-from explanation import get_minimal_explanation
+from explanation import get_minimal_explanation, print_explanation
 
 
 def main():
-    train_path = get_dataset_path('heart_disease', 'train.csv')
+    train_path = get_dataset_path('iris', 'train.csv')
     train_data = pd.read_csv(train_path)
-    test_path = get_dataset_path('heart_disease', 'test.csv')
+    test_path = get_dataset_path('iris', 'test.csv')
     test_data = pd.read_csv(test_path)
     dataframe = pd.concat([train_data, test_data], ignore_index=True)
-    model_path = get_model_path('heart.h5')
+    model_path = get_model_path('iris.h5')
     model = load_model(model_path)
     mdl, output_bounds = build_network(model, dataframe, 'tjeng')
     for data_idx, data in test_data.iterrows():
         network_input = tf.reshape(data.iloc[:-1], (1, -1))
         network_output = np.argmax(model.predict(network_input))
         mdl_clone = mdl.clone()
-        explanation = get_minimal_explanation(mdl_clone, output_bounds, 'tjeng', network_input, network_output, 2)
-        print(f'Explanation for data {data_idx}: {explanation}')
+        explanation = get_minimal_explanation(mdl_clone, output_bounds, 'tjeng', network_input, network_output)
+        print_explanation(data_idx, None, explanation)
 
 
 if __name__ == '__main__':
