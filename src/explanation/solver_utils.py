8f1f64971114ec3abcd9133956077036f9cae1c3
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2022-07-07 16:57:44 +0100

    add function to get input variables

--- src/explanation/solver_utils.py
+++ src/explanation/solver_utils.py
@@ -16,3 +16,17 @@ def get_input_domain_and_bounds(dataframe):
         upper_bound = dataframe[column].max()
         bounds.append([lower_bound, upper_bound])
     return domain, bounds
+
+
+def get_input_variables(mdl, input_domain, input_bounds):
+    input_variables = []
+    for idx, (domain, bounds) in enumerate(zip(input_domain, input_bounds)):
+        lower_bound, upper_bound = bounds
+        name = f'x_{idx}'
+        if domain == 'C':
+            input_variables.append(mdl.continuous_var(lb=lower_bound, ub=upper_bound, name=name))
+        elif domain == 'I':
+            input_variables.append(mdl.integer_var(lb=lower_bound, ub=upper_bound, name=name))
+        else:
+            input_variables.append(mdl.binary_var(name=name))
+    return input_variables
