bda73ba8283b414c519e0a3ae277fa3aa19267fc
Jairo Duarte Ribeiro <jairoduarte123@gmail.com>
2022-07-09 21:34:23 +0100

    add functions to get and print minimal explanation

---
+++ src/explanation/explanation_utils.py
@@ -0,0 +1,28 @@
+from explanation.fischetti import insert_output_constraints_fischetti
+
+
+def log_explanation(logger, features, explanation):
+    def constraint_to_idx(constraint):
+        return int(constraint.lhs.name[2:])
+    columns = [constraint_to_idx(constraint) for constraint in explanation]
+    result = list(features[columns])
+    logger.info(f'Explanation: {result}')
+
+
+def get_minimal_explanation(mdl, bounds, network_input, network_output):
+    number_classes = len(bounds['output'])
+    variables = {
+        'output': [mdl.get_var_by_name(f'o_{class_idx}') for class_idx in range(number_classes)],
+        'binary': mdl.binary_var_list(number_classes - 1, name='b')
+    }
+    input_constraints = mdl.add_constraints(
+        [mdl.get_var_by_name(f'x_{idx}') == feature.numpy() for idx, feature in enumerate(network_input[0])],
+        names='input')
+    mdl.add_constraint(mdl.sum(variables['binary']) >= 1)
+    insert_output_constraints_fischetti(mdl, network_output, variables)
+    for constraint in input_constraints:
+        mdl.remove_constraint(constraint)
+        mdl.solve(log_output=False)
+        if mdl.solution is not None:
+            mdl.add_constraint(constraint)
+    return mdl.find_matching_linear_constraints('input')
